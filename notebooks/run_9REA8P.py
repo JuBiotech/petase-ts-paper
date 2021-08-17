4989e065bf30c5cc468716e78bf64fdca39acc80
Laura Helleckes <l.helleckes@fz-juelich.de>
2021-08-17 13:32:31 +0200

    generalize analysis script for all runs

--- notebooks/run_9REA8P.py
+++ notebooks/run_9REA8P.py
@@ -11,46 +11,63 @@ import robotools
 import cutisplit
 
 
-DP_RUN = pathlib.Path(r"\\IBT705\DATA\CM\Pahpshmir_BL-sGFP-Cutinase-BSI\9REA8P")
-
-
-def get_df_calibration():
-    df_calibration = cutisplit.read_nitrophenol_calibration(DP_RUN)
-    return df_calibration
-
-
-def get_df_inputs():
-    """ Construct a DataFrame of cutinase concentration factors in the "standards"
+class CutisplitAnalysis():
     """
-    # the standards were located in columns [1-3] of the "samples" DWP
-    # they were copied into [4-6] within the "samples" DWP
-    # and again copied into [7-12] of the assay plate
-    plan = robotools.DilutionPlan(
-        xmin=0.01, xmax=1,
-        R=8, C=3,
-        stock=1,
-        mode="log",
-        vmax=950,
-        min_transfer=30
-    )
+    Convenience class to execute the general functions for cutisplit analysis.
+    
+    Attributes
+    ----------
+    dcs_experiment : str
+        User-defined name of the DCS experiment (see IBT705)
+    
+    run_id : str
+        ID of the DCS run to be analysed
+        
+    """
+    def __init__(self, dcs_experiment, run_id):
+        
+        self.run_id = run_id
+        self.dcs_experiment = dcs_experiment
+        self.DP_RUN = pathlib.Path(rf"\\IBT705\DATA\CM\{self.dcs_experiment}\{self.run_id}")
+        pass
 
-    df_inputs = pandas.DataFrame(
-        columns=["input_well", "type", "concentration_factor"]
-    ).set_index("input_well")
-    for ir, r in enumerate("ABCDEFGH"):
-        for ic, c in enumerate([1,2,3,4,5,6]):
-            df_inputs.loc[f"{r}{c:02d}", "type"] = f"reference"
-            df_inputs.loc[f"{r}{c:02d}", "concentration_factor"] = plan.x[ir, ic % 3]
-    return df_inputs
-
-
-def get_df_cutinase():
-    df_cutinase = cutisplit.read_cutinase(DP_RUN)
-    return df_cutinase
+    def get_df_calibration(self):
+        df_calibration = cutisplit.read_nitrophenol_calibration(self.DP_RUN)
+        return df_calibration
 
 
-def get_df_sgfp():
-    df_sgfp, t0_delta = cutisplit.read_sgfp(DP_RUN)
-    print("!! Overriding t0_delta with 0.25 hours !!")
-    t0_delta = 0.25
-    return df_sgfp, t0_delta
+    def get_df_inputs(self):
+        """ Construct a DataFrame of cutinase concentration factors in the "standards"
+        """
+        # the standards were located in columns [1-3] of the "samples" DWP
+        # they were copied into [4-6] within the "samples" DWP
+        # and again copied into [7-12] of the assay plate
+        plan = robotools.DilutionPlan(
+            xmin=0.01, xmax=1,
+            R=8, C=3,
+            stock=1,
+            mode="log",
+            vmax=950,
+            min_transfer=30
+        )
+
+        df_inputs = pandas.DataFrame(
+            columns=["input_well", "type", "concentration_factor"]
+        ).set_index("input_well")
+        for ir, r in enumerate("ABCDEFGH"):
+            for ic, c in enumerate([1,2,3,4,5,6]):
+                df_inputs.loc[f"{r}{c:02d}", "type"] = f"reference"
+                df_inputs.loc[f"{r}{c:02d}", "concentration_factor"] = plan.x[ir, ic % 3]
+        return df_inputs
+
+
+    def get_df_cutinase(self):
+        df_cutinase = cutisplit.read_cutinase(self.DP_RUN)
+        return df_cutinase
+
+
+    def get_df_sgfp(self):
+        df_sgfp, t0_delta = cutisplit.read_sgfp(self.DP_RUN)
+        print("!! Overriding t0_delta with 0.25 hours !!")
+        t0_delta = 0.25
+        return df_sgfp, t0_delta
