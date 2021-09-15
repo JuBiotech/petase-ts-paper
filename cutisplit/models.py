7843bea02026e402e506e65fdf78b8651f8027ee
Laura Helleckes <l.helleckes@fz-juelich.de>
2021-09-15 10:11:21 +0200

    hierarchical model for PETase (draft)

--- cutisplit/models.py
+++ cutisplit/models.py
@@ -359,3 +359,190 @@ class CombinedModel:
                 examples = ", ".join([str(v) for v in vals][:3])
                 examples += f", …, {cvals[-1]}"
             print(f"{cname: <20}{shape}\t{examples}")
+
+class LongFormModel:
+    def __init__(
+        self,
+        df_inputs: pandas.DataFrame,
+        *,
+        df_kinetics: typing.Optional[pandas.DataFrame],
+        cm_nitrophenol: typing.Optional[calibr8.CalibrationModel],
+        sd_cutinase: float = 0.3,
+    ):
+        """Create a model for the (optionally) combined analysis of split-GFP and cutinase assay.
+
+        Parameters
+        ----------
+        df_inputs : pandas.DataFrame
+            A table that describes the layout of the samples that went into the experiment.
+            Index: well ID, corresponding to the well IDs in the DWP
+            Columns:
+                "type" (str) is the kind of sample, for example "reference", "strain5", etc.
+                "concentration_factor" (float) is the concentration in the inpu well. 1 ≙ undiluted
+        df_cutinase : pandas.DataFrame
+            Observations of the cutinase assay.
+            Index: [well, cycle]
+            Columns: [time, value]
+        cm_nitrophenol : calibr8.CalibrationModel
+            The calibration model for 4-nitrophenol absorbance/concentration
+        df_sgfp : pandas.DataFrame
+            Observations of the split-GFP assay
+            Index: time_hours
+            Columns: assay well IDs
+        t0_delta : float
+            Time delay (in hours) between pipetting and first measurement of the split-GFP assay
+        sd_cutinase : float
+            Approximate pipetting error from input well to cutinase assay well.
+            0.1 ≙ 10 % relative error
+        sd_sgfp : float
+            Approximate pipetting error from input well to split-GFP assay well.
+            0.1 ≙ 10 % relative error
+        """
+        #TODO: self.df_inputs = _validate_df_inputs(df_inputs.copy())
+
+        #TODO: if df_cutinase is not None:
+        #     df_cutinase = _validate_df_cutinase(df_cutinase)
+        self.df_inputs = df_inputs
+        self.df_kinetics = df_kinetics
+        self.sd_cutinase = sd_cutinase
+        self.cm_nitrophenol = cm_nitrophenol
+        self.idx_CIDtoS = None
+        self.idx_KIDtoCID = None
+
+        with pymc3.Model() as self.pmodel:
+            self._model_concentrations()
+            self._model_cutinase()
+        super().__init__()
+
+    def _model_concentrations(self):
+        df_inputs = self.df_inputs
+
+        # first check that the input dataframe corresponds to the standard format:
+
+        # input checks successful! Can now start building the model...
+        # First get a handle on the PyMC3 model
+        pmodel = pymc3.modelcontext(None)
+
+        
+        strains = list(sorted(set(df_inputs.strain)))
+        culture_ids = list(sorted(set(df_inputs.index.to_numpy(dtype=str))))
+        kinetic_ids = list(sorted(set(self.df_kinetics.index.to_numpy(dtype=str))))
+
+        S = "strain"
+        CID = "culture_id"
+        KID = "kinetic_id"
+
+        # The model describes the data in the "long form". It needs some lists for indexing to map
+        # between variables of different dimensionality.
+        # For example the `idx_CIDtoC` maps each culture id (CID) to the 0-based index of the corresponding condition (C).
+        self.idx_CIDtoS = [
+            strains.index(row.strain)
+            for row in df_inputs.loc[culture_ids].itertuples()
+        ]
+        assert numpy.shape(self.idx_CIDtoS) == (len(culture_ids),)
+        assert max(self.idx_CIDtoS) == len(strains) - 1
+
+        self.idx_KIDtoCID = [
+            culture_ids.index(row.culture_id)
+            for row in self.df_kinetics.loc[kinetic_ids].itertuples()
+        ]
+        assert numpy.shape(self.idx_KIDtoCID) == (len(kinetic_ids),), f"{numpy.shape(self.idx_KIDtoCID)}, {len(kinetic_ids)}"
+        assert max(self.idx_KIDtoCID) == len(culture_ids) - 1
+
+        # Now create "coords" that describe the relevant dimensions
+        _add_or_assert_coords(
+            {
+                "strain": numpy.array(strains, dtype=str),
+                "culture_id": numpy.array(culture_ids, dtype=str),
+                "kinetic_id": numpy.array(kinetic_ids, dtype=str)
+            },
+            pmodel,
+        )
+
+        cf_input = pymc3.Data(
+            "cf_input", self.df_kinetics.concentration_factor.to_numpy(dtype=float), dims=("kinetic_id",)
+        )
+        cf_cutinase_assay = pymc3.Lognormal(
+            "cf_cutinase_assay",
+            mu=at.log(cf_input),
+            sd=self.sd_cutinase,
+            dims=("kinetic_id",),
+        )
+
+        return
+
+    def _model_cutinase(self):
+        t_obs = numpy.vstack(self.df_kinetics.sort_index().time)
+        y_obs = numpy.vstack(self.df_kinetics.sort_index().value)
+
+        pmodel = pymc3.modelcontext(None)
+        _add_or_assert_coords(
+            {
+                "cutinase_cycle": numpy.arange(t_obs.shape[1]),
+            },
+            pmodel,
+        )
+
+        cf_input = pmodel["cf_input"]
+        cf_cutinase_assay = pmodel["cf_cutinase_assay"]
+
+        # For the cutinase assay, we introduce the specific enzyme activity in the supernatant [µmol/mL/min].
+        # Again, we assume one undiluted activity and calculate the activities in the sample/replicate wells
+        # deterministically from the relative concentrations above.
+        k = pymc3.HalfNormal("k", sd=3, dims=("strain",), )
+        k_batch = pymc3.Lognormal(
+            "k_batch",
+            mu=at.log(k[self.idx_CIDtoS]),
+            sd=0.1,
+            dims=("culture_id")
+        )
+        # The inputs are diluted 500x into the assay.
+        dilution_factor = pymc3.Data("dilution_factor", 72)
+        k_assay = pymc3.Lognormal(
+            "k_assay",
+            mu=at.log(cf_cutinase_assay * k_batch[self.idx_KIDtoCID] / dilution_factor),
+            sd=0.05,
+            dims=("kinetic_id",),
+        )
+
+        # prediction
+        cutinase_time = pymc3.Data(
+            "cutinase_time", t_obs, dims=("kinetic_id", "cutinase_cycle")
+        )
+        #P0 = pymc3.Uniform("P0", 0, 0.4, dims=("assay_well",))
+        S0 = pymc3.Uniform("S0", 0.5, 0.7) #truth should be 0.662 mM
+        #time_delay = pymc3.Normal("time_delay", sd=2, dims=("assay_well"))
+
+        product_concentration = pymc3.Deterministic(
+            "product_concentration",
+            S0 * (1 - at.exp(-k_assay[:, None] * cutinase_time )),
+            dims=("kinetic_id", "cutinase_cycle"),
+        )
+        absorbance_intercept = pymc3.Normal("absorbance_intercept", mu=self.cm_nitrophenol.theta_fitted[0], sd=1, dims=("kinetic_id"))
+
+        # The reaction product (4-nitrophenol) is measured in [mmol/L] = [µmol/mL] via the error model.
+        # Our prediction is also in µmol/mL.
+        absorbance = pymc3.Data(
+            "cutinase_absorbance", y_obs , dims=("kinetic_id", "cutinase_cycle")
+        )
+        L_cut = self.cm_nitrophenol.loglikelihood(
+            y=absorbance,
+            x=product_concentration,
+            name="cutinase_all",
+            dims=("kinetic_id", "cutinase_cycle"),
+            dependent_key=self.cm_nitrophenol.dependent_key,
+            theta=[absorbance_intercept[:, None], *self.cm_nitrophenol.theta_fitted[1:]]
+        )
+        return
+
+ 
+    def summary(self):
+        for cname, cvals in self.pmodel.coords.items():
+            shape = numpy.shape(cvals)
+            vals = numpy.asarray(cvals).flatten()
+            if numpy.prod(shape) < 5:
+                examples = ", ".join([str(v) for v in vals])
+            else:
+                examples = ", ".join([str(v) for v in vals][:3])
+                examples += f", …, {cvals[-1]}"
+            print(f"{cname: <20}{shape}\t{examples}")
